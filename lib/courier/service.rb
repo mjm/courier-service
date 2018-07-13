8d82e196404acd4c126aef2dd1695e549b79782a
Matt Moriarity <matt@mattmoriarity.com>
2018-07-13 10:50:50 -0500

    Add logic for loading cloud environment

--- lib/courier/service.rb
+++ lib/courier/service.rb
@@ -1,7 +1,61 @@
-require "courier/service/version"
+require 'courier/service/version'
 
 module Courier
   module Service
-    # Your code goes here...
+    BUCKET_KEY = 'GOOGLE_CLOUD_STORAGE_BUCKET'.freeze
+
+    def self.load_environment_variables(
+      adapter: GoogleStorageAdapter.new(ENV[BUCKET_KEY])
+    )
+      new_env = ServiceEnvironment.new(env: ENV, adapter: adapter)
+      ENV.replace(new_env.environment)
+    end
+  end
+
+  class ServiceEnvironment
+    def initialize(env: ENV, adapter:)
+      @env = env
+      @adapter = adapter
+    end
+
+    def rack_env
+      (@env['RACK_ENV'] || 'development').to_sym
+    end
+
+    def production?
+      rack_env == :production
+    end
+
+    def environment
+      return @env unless production?
+      env = @env.dup
+
+      @adapter.file('.envrc').each_line do |line|
+        /^export (\w+)="(.*)"$/.match line do |m|
+          env[m[1]] = m[2]
+        end
+      end
+
+      env
+    end
+  end
+
+  class GoogleStorageAdapter
+    def initialize(bucket_name)
+      @bucket_name = bucket_name
+    end
+
+    def bucket
+      @bucket ||= begin
+                    require 'google/cloud/storage'
+                    Google::Cloud::Storage.new.bucket(@bucket_name)
+                  end
+    end
+
+    def file(name)
+      file = bucket.file(name).download
+      file.rewind
+      file
+    end
   end
 end
--- spec/courier/service_spec.rb
+++ spec/courier/service_spec.rb
@@ -2,4 +2,37 @@ RSpec.describe Courier::Service do
   it 'has a version number' do
     expect(Courier::Service::VERSION).not_to be nil
   end
+
+  describe 'loading environment variables' do
+    let(:adapter) { double(:adapter, file: file) }
+    let(:file) do
+      StringIO.new <<~ENV
+        export FOO="baz"
+        export A_NEW_VAR="my dude"
+        export MOTD="this file can hold so many env vars"
+        ENV
+    end
+    subject { Courier::ServiceEnvironment.new(env: env, adapter: adapter) }
+
+    context 'when running in the development environment' do
+      let(:env) { { 'RACK_ENV' => 'development', 'FOO' => 'bar' } }
+
+      it 'does not alter the environment' do
+        expect(subject.environment).to match env
+      end
+    end
+
+    context 'when running in the production environment' do
+      let(:env) { { 'RACK_ENV' => 'production', 'FOO' => 'bar' } }
+
+      it 'merges in variables from the .envrc file' do
+        expect(subject.environment).to match({
+          'RACK_ENV' => 'production',
+          'FOO' => 'baz',
+          'A_NEW_VAR' => 'my dude',
+          'MOTD' => 'this file can hold so many env vars'
+        })
+      end
+    end
+  end
 end
