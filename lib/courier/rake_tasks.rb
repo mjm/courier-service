43c303eb9bb23eccb99df371695e349498045f77
Matt Moriarity <matt@mattmoriarity.com>
2018-07-29 10:00:46 -0500

    Update proto task to copy files to client
    Only do this if the service has the directory for a client gem.

--- lib/courier/rake_tasks.rb
+++ lib/courier/rake_tasks.rb
@@ -1,3 +1,11 @@
 task :proto do
   sh 'protoc --ruby_out=. --twirp_ruby_out=. --doc_out=doc/ --doc_opt=html,index.html app/service/service.proto'
+
+  project = File.basename(Dir.pwd)
+  gem_dir = project.tr('-', '/')
+  if Dir.exist? 'client'
+    dest = "client/lib/#{gem_dir}"
+    mkdir_p dest
+    cp_r Dir['app/service/*.rb'], dest
+  end
 end
