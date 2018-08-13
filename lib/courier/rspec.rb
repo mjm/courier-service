8b6319b3cb61033be577a2b7c60f1e0964559e92
Matt Moriarity <matt@mattmoriarity.com>
2018-08-12 21:46:56 -0500

    Add some helpers for Twirp handler tests

--- lib/courier/rspec.rb
+++ lib/courier/rspec.rb
@@ -15,3 +15,44 @@ RSpec::Matchers.define :be_a_twirp_error do |code, msg = nil|
     end
   end
 end
+
+RSpec.shared_examples 'an unauthenticated request' do
+  context 'when no auth token is provided' do
+    let(:env) { {} }
+
+    it 'returns an unauthenticated response' do
+      expect(response).to be_a_twirp_error :unauthenticated
+    end
+  end
+end
+
+RSpec.shared_examples 'a request from another user' do
+  context 'when an auth token from a different user is provided' do
+    let(:env) { { token: other_token } }
+
+    it 'returns a forbidden response' do
+      expect(response).to be_a_twirp_error :permission_denied
+    end
+  end
+end
+
+module Courier
+  module RPCHelpers
+    def rpc_method(name)
+      let(:method_name) { name }
+      let(:response) { subject.call_rpc(method_name, request, env) }
+    end
+
+    def self.extend_object(base)
+      super
+      base.let(:request) { {} }
+      base.let(:token) do
+        Courier::Middleware::JWTToken.new('sub' => 'example', 'uid' => 123)
+      end
+      base.let(:other_token) do
+        Courier::Middleware::JWTToken.new('sub' => 'example2', 'uid' => 124)
+      end
+      base.let(:env) { { token: token } }
+    end
+  end
+end
